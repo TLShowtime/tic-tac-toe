313e5fa5222fcfb7d4e316f1134831990c412e80
TLShowtime <liuguo35@hotmail.com>
2023-01-10 16:13:25 -0600

    Changed attributes of class Game in typescript

--- src/functions/Board.ts
+++ src/functions/Board.ts
@@ -1,23 +1,17 @@
 export class Game {
-  constructor() {
-    this.winner = null
-    this.board = [
-      ['', '', ''],
-      ['', '', ''],
-      ['', '', '']
-    ]
-    this.player = 'X'
-    this.winning_lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ]
-  }
+  winner: string | null = null
+  board: string[][] = [['', '', ''],['', '', ''],['', '', '']]
+  player: string = 'X'
+  winning_lines: number[][] = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
 
   changePlayer() {
     this.player = this.player === 'O' ? 'X' : 'O'
@@ -33,7 +27,7 @@ export class Game {
     })
   }
 
-  move(x, y) {
+  move(x: number, y: number) {
     if (this.winner) return
     if (this.board[x][y] != '') return
     this.board[x][y] = this.player
